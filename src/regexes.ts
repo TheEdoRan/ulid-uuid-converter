7879157c08e4cec04a63d98a84d2914b047c8200
Edoardo Ranghieri <edoardo@ranghieri.com>
2023-02-23 03:14:49 +0100

    fix: ulid regex

--- src/regexes.ts
+++ src/regexes.ts
@@ -1,3 +1,3 @@
-export const ULID_REGEX = /^[0-9a-hjkmnp-tv-zA-HJKMNP-TV-Z]{26}$/;
+export const ULID_REGEX = /^[0-7][0-9a-hjkmnp-tv-zA-HJKMNP-TV-Z]{25}$/;
 
 export const UUID_REGEX = /^[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$/;
